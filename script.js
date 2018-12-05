52467214cc795669476412fba209e3f0268ea09c
drood <dbernstein1987@hotmail.com>
2018-12-05 19:43:58 +1100

    crazy box shadow movement

---
+++ script.js
@@ -0,0 +1,29 @@
+const hero = document.querySelector(".hero");
+const text = hero.querySelector("h1");
+const walk = 100; // 100px
+
+function shadow(e) {
+    // const width = hero.offsetWidth;
+    // const height = hero.offsetHeight;
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    let { offsetX: x, offsetY: y } = e;
+    // console.log(this, e.target); // 'this' will be always element we are listening to and 'e.target' the element we actually hovering over
+    if (this !== e.target) {
+        x = x + e.target.offsetLeft;
+        y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width) * walk - walk / 2);
+    const yWalk = Math.round((y / width) * walk - walk / 2);
+    console.log(xWalk, yWalk);
+
+    text.style.textShadow = `${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+                             ${xWalk *
+                                 -1}px ${yWalk}px 0 rgba(0, 255, 255, 0.7),
+                                 ${yWalk}px ${xWalk *
+        -1}px 0 rgba(0, 255, 0, 0.7),
+                                 ${yWalk *
+                                     -1}px ${xWalk}px 0 rgba(0, 233, 255, 0.7)`;
+}
+
+hero.addEventListener("mousemove", shadow);
